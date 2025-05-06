ad3f7066c98da24aa9d2c7aba9acec7d6de2c9cc
Stavros Pontikis <stavros.pontikis@gmail.com>
2025-05-06 19:15:25 +0300

    Minor fixes

--- sql/Q05.sql
+++ sql/Q05.sql
@@ -1,5 +1,5 @@
 /* QUERY 05 - Βρείτε τους νέους καλλιτέχνες (ηλικία < 30 ετών) που έχουν τις περισσότερες συμμετοχές σε φεστιβάλ;*/
-select a.stage_name, a.date_of_birth, count(e.festival_id) as festival_appearences --count(p.performance_id), e.festival_id
+select a.stage_name, a.date_of_birth, count(distinct e.festival_id) as festival_appearences --count(p.performance_id), e.festival_id
 from artist a 
 join performance p on (p.artist_id = a.artist_id)
 join "event" e on (e.event_id = p.event_id)
