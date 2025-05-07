6adff49ae69c7876c7484efe6caa80f3ba3fcc12
Stavros Pontikis <stavros.pontikis@gmail.com>
2025-05-07 14:17:08 +0300

    Update Q11.sql

--- sql/Q11.sql
+++ sql/Q11.sql
@@ -1,13 +1,13 @@
 /* QUERY 11 - Βρείτε όλους τους καλλιτέχνες που συμμετείχαν τουλάχιστον 5 λιγότερες φορές από τον καλλιτέχνη με τις
 περισσότερες συμμετοχές σε φεστιβάλ.*/
 with festival_participations as (
-	select a.artist_id, count(e.festival_id) as appearences
+	select a.artist_id, a.stage_name, count(e.festival_id) as appearences
 	from artist a 
 	join performance p on p.artist_id = a.artist_id 
 	join event e on e.event_id = p.event_id 
 	group by a.artist_id 
 	order by 1, 2
 )
-select fp.artist_id, fp.appearences
+select fp.artist_id, fp.stage_name, fp.appearences as number_of_appearences
 from festival_participations fp
 where fp.appearences < (select max(fp2.appearences) from  festival_participations fp2) - 4
